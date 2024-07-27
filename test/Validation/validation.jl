914b76f0a451f2cb1ddca4ba9fc26d3c06feb294
maysam-gholampour <meysam.gholampoor@gmail.com>
2024-07-27 18:25:26 +0800

    feat: Refactor ionic_liquid_coil_ode! function and update validation test data for evaporator and condenser, code working well

--- test/Validation/validation.jl
+++ test/Validation/validation.jl
@@ -8,8 +8,8 @@ begin "constants"
     const g = 9.81
     const H = 0.132
     const FS = 0.00254
-    const Le = 0.8
-    NTUᴰₐᵢᵣ = 2.125
+    const Le = 0.85
+    NTUᴰₐᵢᵣ = 1.0
     const σ = 1.0
 end
 
@@ -18,14 +18,14 @@ begin "validate evaporator 1"
     T_air_amb = 25.48 + 273.15  # K
     T_wb_air_amb = 21.21 + 273.15  # K
     m_dot_air_deh = 0.03584  # kg/s
-    m_dot_sol_deh = 2.57 / 60 / 1000  # kg/s
+    m_dot_sol_deh = 1100 * 2.57 / 60 / 1000  # kg/s  #FIXME: 1100 is density
     T_sol_in_deh = 21 + 273.15  # K
     X_sol_in_deh = 0.76
     T_ref_in_evap = 12.19 + 273.15  # K
     T_ref_out_evap = 17.04 + 273.15  # K
     P_evap = 4.3 * 100000  # Pa
     T_sat_evap = 10.51 + 273.15  # K
-    Q_evap = 1192  # W
+    Q_evap = 862  # W
     # ========================================
     ṁₐ = (m_dot_air_deh / N_fin_evap) * 0.5 # mass flow rate for half of the fin space
     ṁₛₒₗ = (m_dot_sol_deh / N_fin_evap) * 0.5 # mass flow rate for half of the fin space
@@ -68,14 +68,64 @@ begin "validate evaporator 1"
     # res = solve_coil_ode(IL ,H ,Le ,∂Qᵣ ,ṁₐᵢᵣ_ᵢₙ ,NTUᴰₐᵢᵣ ,σ ,ṁₛₒₗ_ᵢₙ ,ξₛₒₗ_ᵢₙ ,iₛₒₗ_ᵢₙ , ωₐ_ᵢₙ, iₐ_ᵢₙ)
 end
 
-@show ṁₛₒₗ
-@show ṁₐᵢᵣ_ᵢₙ
-@show iₐ_ᵢₙ
-@show ξₛₒₗ_ᵢₙ
-@show iₛₒₗ_ᵢₙ
-@show ωₐ_ᵢₙ
+@show ṁₛₒₗ / ṁₐᵢᵣ_ᵢₙ 
+@show MR = ṁₛₒₗ_ᵢₙ / ṁₐᵢᵣ_ᵢₙ
+@show ER = iₛₒₗ_ᵢₙ / iₐ_ᵢₙ
+sol = solve_coil_ode(IL ,H ,Le ,∂Qᵣ ,ṁₐᵢᵣ_ᵢₙ ,NTUᴰₐᵢᵣ ,σ ,ṁₛₒₗ_ᵢₙ ,ξₛₒₗ_ᵢₙ ,iₛₒₗ_ᵢₙ , ωₐ_ᵢₙ, iₐ_ᵢₙ)
 
-res = solve_coil_ode(IL ,H ,Le ,∂Qᵣ ,ṁₐᵢᵣ_ᵢₙ ,NTUᴰₐᵢᵣ ,σ ,ṁₛₒₗ_ᵢₙ ,ξₛₒₗ_ᵢₙ ,iₛₒₗ_ᵢₙ , ωₐ_ᵢₙ, iₐ_ᵢₙ)
+size(sol.u[1])
+x1 = zeros(length(sol.u))
+x2 = zeros(length(sol.u))
+x3 = zeros(length(sol.u))
+x4 = zeros(length(sol.u))
+x5 = zeros(length(sol.u))
+for i in 1:length(sol.u)
+    # ωₐᵢᵣ, iₐᵢᵣ, ṁₛₒₗ,ξₛₒₗ, iₛₒₗ = u
+    x1[i] = sol.u[i][1] * ωₐ_ᵢₙ
+    x2[i] = sol.u[i][2] * iₐ_ᵢₙ
+    x3[i] = sol.u[i][3] * ṁₛₒₗ_ᵢₙ
+    x4[i] = sol.u[i][4] * ξₛₒₗ_ᵢₙ
+    x5[i] = sol.u[i][5] * iₛₒₗ_ᵢₙ
+end
+
+using Plots
+x1 = reverse(x1)
+x2 = reverse(x2)
+
+T_air_out_deh = 19.57 + 273.15 # K
+RH_out_deh = 0.01 * 49.61 # % 
+@show ωₐ_out = HAPropsSI("W", "T", T_air_out_deh, "P", 101325.0 , "R", RH_out_deh)
+x1[end]
+@show iₐ_out = HAPropsSI("H", "T", T_air_out_deh, "P", 101325.0 , "W", ωₐ_out)
+x2[end]
+plot(sol.t, x1, label = "ωᵢᵣ")
+plot(sol.t, x2, label = "iᵢᵣ")
+plot(sol.t, x3, label = "ṁₛₒₗ")
+plot(sol.t, x4, label = "ξₛₒₗ")
+plot(sol.t, x5, label = "iₛₒₗ")
+T_sol_calc = (i,ξ) -> calculate_T_sol(i, ξ,IL) - 273.15
+T_sol = @. T_sol_calc(x5, x4)
+plot(sol.t, T_sol, label = "Tₛₒₗ")
+
+calculate_T_air = (ω,i) -> HAPropsSI("T", "W", ω , "H", i, "P", 101325.0)
+T_air  = @. calculate_T_air(x1,x2) - 273.15
+plot(sol.t, T_air, label = "Tᵢᵣ")
+
+calculate_T_dp = (ω,i) -> HAPropsSI("Tdp", "W", ω , "H", i, "P", 101325.0)
+T_dp  = @. calculate_T_dp(x1,x2) - 273.15
+plot(sol.t, T_dp, label = "Tdp")
+
+calculate_T_wet = (ω,i) -> HAPropsSI("Twb", "W", ω , "H", i, "P", 101325.0)
+T_wet  = @. calculate_T_wet(x1,x2) - 273.15
+plot(sol.t, T_wet, label = "Twb")
+
+calculate_RH = (ω,i) -> HAPropsSI("R", "W", ω , "H", i, "P", 101325.0)
+RH  = @. calculate_RH(x1,x2)
+plot(sol.t, RH, label = "RH")
+
+i_a_in = HAPropsSI("H", "T", T_air_amb, "P", 101325.0 , "Twb", T_wb_air_amb)
+i_a_out = HAPropsSI("H", "T", T_air_out_deh, "P", 101325.0 , "R", RH_out_deh)
+ṁₐᵢᵣ_ᵢₙ * (i_a_in - i_a_out) * 2 * N_fin_evap
 
 
 begin "test evaporator 2 "
